f771145b9f2dea98c34a794e19d0d3006e872b16
Simon Raes <simonraes@gmail.com>
2014-06-07 11:31:45 +0200

    First commit

---
+++ Telemeter/Telemeter/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Telemeter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Login: <asp:TextBox ID="txtLogin" runat="server"></asp:TextBox><br />
+        Password: <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btnGo" runat="server" Text="Get information" OnClick="btnGo_Click" /><br />
+        <br />
+        Total usage: <asp:Label ID="lblTotalUsed" runat="server" Text=""></asp:Label>
+    &nbsp;(out of maximum <asp:Label ID="lblMaxOne" runat="server" Text="Label"></asp:Label>&nbsp;-&nbsp;<asp:Label ID="lblMaxTwo" runat="server" Text="Label"></asp:Label>)
+        <br />
+        Period: <asp:Label ID="lblFrom" runat="server" Text="Label"></asp:Label>&nbsp;-&nbsp;<asp:Label ID="lblTill" runat="server" Text="Label"></asp:Label>
+        <br />
+        Status:
+        <asp:Label ID="lblStatus" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Telemeter/Telemeter/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telemeter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TelemeterService.RetrieveUsageRequestType req = new TelemeterService.RetrieveUsageRequestType();
+                req.UserId = txtLogin.Text;
+                req.Password = txtPassword.Text;
+
+                TelemeterService.TelemeterService service = new TelemeterService.TelemeterService();
+                TelemeterService.RetrieveUsageResponseType response = service.retrieveUsage(req);
+                String telenetType = response.Item.ToString();
+
+                if (telenetType.Equals("Telemeter.TelemeterService.FUPType"))
+                {
+                    DateTime day = response.Ticket.ExpiryTimestamp;
+                    TelemeterService.FUPType fupType = (TelemeterService.FUPType)response.Item;
+
+                    //usage
+                    decimal totalUsed = fupType.Usage.TotalUsage;
+                    decimal maxMin = fupType.Usage.MinUsageRemaining;
+                    decimal maxMax = fupType.Usage.MaxUsageRemaining;
+                    lblTotalUsed.Text = totalUsed.ToString() + fupType.Usage.Unit.ToString();
+                    lblMaxOne.Text = maxMin.ToString() + fupType.Usage.Unit.ToString();
+                    lblMaxTwo.Text = maxMax.ToString() + fupType.Usage.Unit.ToString();
+
+                    //period
+                    DateTime from = fupType.Period.From;
+                    DateTime till = fupType.Period.Till;
+                    lblFrom.Text = from.Date.ToString();
+                    lblTill.Text = till.Date.ToString();
+
+                    //status
+                    String status = fupType.Status.ToString();
+                    lblStatus.Text = fupType.Status.ToString();
+
+                    if (status.Equals("Grootverbruik"))
+                    {
+                        //Extra info only available in case of "Grootverbruik" status
+                        DateTime startVrijVerbruik = fupType.Info.StartVrijVerbruik;
+                        DateTime startGrootverbruiker = fupType.Info.StartGrootverbruiker;
+                    }
+                }
+                else
+                {
+                    //Not using FUP
+                    TelemeterService.VolumeType volumeType = (TelemeterService.VolumeType)response.Item;
+                    TelemeterService.DailyUsageType[] dailyUsages = volumeType.DailyUsageList;
+                }
+            }
+            catch (Exception ex)
+            {
+                //should only catch SOAPException
+                lblStatus.Text = ex.Message;
+            }
+        }
+
+    }
+}
